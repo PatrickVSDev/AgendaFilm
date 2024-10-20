7b284bf82323882edad3aa404ed8ae72d1810c1a
Patrick Pereira <patrickcianorte@gmail.com>
2024-10-20 15:50:25 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaFilm/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AgendaFilm
+{
+    partial class PaginaLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaginaLogin));
+            pboxLogo = new PictureBox();
+            Usuario = new Label();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            Senha = new Label();
+            groupBox1 = new GroupBox();
+            btAcessar = new Button();
+            btCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pboxLogo).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pboxLogo
+            // 
+            pboxLogo.Image = (Image)resources.GetObject("pboxLogo.Image");
+            pboxLogo.Location = new Point(83, 22);
+            pboxLogo.Name = "pboxLogo";
+            pboxLogo.Size = new Size(269, 204);
+            pboxLogo.TabIndex = 0;
+            pboxLogo.TabStop = false;
+            pboxLogo.Click += pictureBox1_Click;
+            // 
+            // Usuario
+            // 
+            Usuario.AutoSize = true;
+            Usuario.Location = new Point(83, 249);
+            Usuario.Name = "Usuario";
+            Usuario.Size = new Size(50, 15);
+            Usuario.TabIndex = 1;
+            Usuario.Text = "Usuário:";
+            Usuario.Click += label1_Click;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(139, 246);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(213, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(139, 275);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(213, 23);
+            txtSenha.TabIndex = 4;
+            txtSenha.UseSystemPasswordChar = true;
+            // 
+            // Senha
+            // 
+            Senha.AutoSize = true;
+            Senha.Location = new Point(91, 278);
+            Senha.Name = "Senha";
+            Senha.Size = new Size(42, 15);
+            Senha.TabIndex = 3;
+            Senha.Text = "Senha:";
+            Senha.Click += label1_Click_1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btAcessar);
+            groupBox1.Controls.Add(btCancelar);
+            groupBox1.Controls.Add(pboxLogo);
+            groupBox1.Controls.Add(Usuario);
+            groupBox1.Controls.Add(txtSenha);
+            groupBox1.Controls.Add(txtUsuario);
+            groupBox1.Controls.Add(Senha);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(445, 365);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Formulário de Login";
+            // 
+            // btAcessar
+            // 
+            btAcessar.Location = new Point(277, 314);
+            btAcessar.Name = "btAcessar";
+            btAcessar.Size = new Size(75, 23);
+            btAcessar.TabIndex = 7;
+            btAcessar.Text = "Acessar";
+            btAcessar.UseVisualStyleBackColor = true;
+            btAcessar.Click += btAcessar_Click;
+            // 
+            // btCancelar
+            // 
+            btCancelar.Location = new Point(196, 314);
+            btCancelar.Name = "btCancelar";
+            btCancelar.Size = new Size(75, 23);
+            btCancelar.TabIndex = 6;
+            btCancelar.Text = "Cancelar";
+            btCancelar.UseVisualStyleBackColor = true;
+            btCancelar.Click += btCancelar_Click;
+            // 
+            // PaginaLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 384);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "PaginaLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AgendaFilm";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pboxLogo).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pboxLogo;
+        private Label Usuario;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+        private Label Senha;
+        private GroupBox groupBox1;
+        private Button btCancelar;
+        private Button btAcessar;
+    }
+}
---
+++ AgendaFilm/Form1.cs
@@ -0,0 +1,64 @@
+namespace AgendaFilm
+{
+    public partial class PaginaLogin : Form
+    {
+        public PaginaLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btAcessar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsuario.Text.Equals("admin") && txtSenha.Text.Equals("admin"))
+                {
+                    var menu = new PaginaRestrita();
+                    menu.Show();
+
+                    this.Visible = false;
+
+                }else{
+                    MessageBox.Show("Usuário, ou senha incorreto.",
+                                    "Desculpe.",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    txtUsuario.Focus();
+                    txtSenha.Text = "";
+                }
+            }
+            catch(Exception ex) {
+                MessageBox.Show("Desculpe.",
+                                ex.Message,
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+    }
+}
